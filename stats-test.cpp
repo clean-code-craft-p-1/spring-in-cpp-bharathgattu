5b2e2ff8992dcbf773928609f92d8eab874b62ce
Gattu <Bharath.Gattu@philips.com>
2023-07-23 18:50:32 +0530

    StatisticsEmailAlerterAssignment-initial

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,3 +1,4 @@
+//#include "pch.h"
 #include "stats.h"
 
 #include "gtest/gtest.h"
@@ -5,7 +6,7 @@
 
 TEST(Statistics, ReportsAverageMinMax) {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    float epsilon = 0.001;
+    double epsilon = static_cast<double>(0.001);
     EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
     EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
     EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
@@ -13,11 +14,8 @@ TEST(Statistics, ReportsAverageMinMax) {
 
 TEST(Statistics, AverageNaNForEmpty) {
     auto computedStats = Statistics::ComputeStatistics({});
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
-    
-    //Design the REQUIRE statement here.
-    //Use http://www.cplusplus.com/reference/cmath/isnan/
+    EXPECT_EQ(std::isnan(0.0), false);
+    EXPECT_EQ(std::isnan(std::sqrt(-1)), true);
 }
 
 TEST(Alert, AlertsWhenMaxExceeds) {
@@ -25,7 +23,7 @@ TEST(Alert, AlertsWhenMaxExceeds) {
     LEDAlert ledAlert;
     std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
     
-    const float maxThreshold = 10.2;
+    const double maxThreshold = static_cast<double>(10.2);
     StatsAlerter statsAlerter(maxThreshold, alerters);
     statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
 
--- stats.h
+++ stats.h
@@ -1,7 +1,49 @@
+//#include "pch.h"
 #include <vector>
+
+class Stats
+{
+public:
+    Stats() :average(0), min(0), max(0) {
+    }
+
+    double calculateAverage(const std::vector<double>& listOfNums);
+    double findMax(const std::vector<double>& listOfNums);
+    double findMin(const std::vector<double>& listOfNums);
+
+    double average;
+    double min;
+    double max;
+};
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::initializer_list<double>);
+
 }
+
+    class IAlerter {
+
+    };
+
+    class StatsAlerter {
+        double max_threshold;
+        std::vector<IAlerter*> alerters;
+    public:
+        StatsAlerter() = default;
+        StatsAlerter(const double threshold, const std::vector<IAlerter*> alerters):max_threshold(threshold),alerters(alerters) {
+            //Constructor with two arguments
+        }
+        void checkAndAlert(const std::vector<double> alerterList);
+    };
+
+    class EmailAlert : public IAlerter {
+    public:
+        static bool emailSent;
+    };
+
+    class LEDAlert : public IAlerter {
+    public:
+        static bool ledGlows;
+    };
+
